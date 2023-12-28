5ac1040576b8fe4c174f0632d651bf2f1f50e3f5
gajurelkshitiz <kshitiz.purbct042@gmail.com>
2023-12-28 16:56:26 +0545

    data ingestion added

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
-from src.kidneytumorclassifier import logger
+from kidneytumorclassifier import logger
+from kidneytumorclassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("Welcome to our Custom Log.")
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+    logger.info(f">>>>>>>> stage {STAGE_NAME} started <<<<<<<<")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>>>>> stage {STAGE_NAME} completed <<<<<<<<<\n\nx===================x")
+
+except Exception as e:
+    logger.exception(e)
+    raise e
