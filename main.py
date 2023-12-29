4fb226430a03fa7001c4569a5b109097a5df76cb
gajurelkshitiz <kshitiz.purbct042@gmail.com>
2023-12-29 18:02:56 +0545

    Training model added

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from kidneytumorclassifier import logger
 from kidneytumorclassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from kidneytumorclassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from kidneytumorclassifier.pipeline.stage_03_model_training import ModelTrainingPipeline
 
 
 STAGE_NAME = "Data Ingestion stage"
@@ -23,6 +24,20 @@ try:
     prepare_base_model = PrepareBaseModelTrainingPipeline()
     prepare_base_model.main()
     logger.info(f">>>>>>>> stage {STAGE_NAME} completed <<<<<<<<<\n\nx===================x")
+
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+
+STAGE_NAME = "Model Training stage"
+try:
+        logger.info("************************")
+        logger.info(f">>>>>>>> stage {STAGE_NAME} started <<<<<<<<")
+        model_training = ModelTrainingPipeline()
+        model_training.main()
+        logger.info(f">>>>>>>> stage {STAGE_NAME} completed <<<<<<<<<\n\nx===================x")
 
 except Exception as e:
     logger.exception(e)
